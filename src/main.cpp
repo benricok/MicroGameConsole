77617c69e1a8065128b77224d8de6a2e0bd17a62
benricok <62467535+benricok@users.noreply.github.com>
2021-08-28 19:17:29 +0200

    add licence

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,21 @@
+// -----------------------------------------------------------------------------
+//
+//  Copyright (C) 2021  Benrico Krog
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published
+//  by the Free Software Foundation version 3 of the License.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>
+//
+// -----------------------------------------------------------------------------
+
 #include <main.h>
 
 boolean booted = false;
@@ -23,7 +41,7 @@ void setup()
   d.fillScreen(BLACK);
   drawCourt();
 
-    initPong();
+  initPong();
 }
 
 void loop()
